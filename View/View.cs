68a57e401c5a5e259c04aca91637b3249e6df1f3
Amaliebra <amaliebratland@gmail.com>
2024-11-03 15:34:23 +0100

    small changes

--- View/View.cs
+++ View/View.cs
@@ -1,3 +1,25 @@
 using Meetingplanner.Controller;
 
 namespace Meetingplanner.View;
+
+public class MeetingView
+{
+    private readonly MeetingController _controller;
+
+    public MeetingView(MeetingController controller)
+    {
+        _controller = controller;
+    }
+
+    public void AddMeeting()
+    {
+        Console.WriteLine("Enter meeting title:");
+        string Title = Console.ReadLine()?.Trim() ?? "Unknown meeting name";
+
+        Console.WriteLine("Enter start time for meeting (yyyy-mm-dd hh-mm)");
+
+
+
+    }
+}
+
