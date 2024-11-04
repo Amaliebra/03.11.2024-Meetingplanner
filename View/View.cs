40e98c3c48a0f57df07177a72d3f7f4d196521a5
Amaliebra <jba082024@gmail.com>
2024-11-04 14:56:23 +0100

    end of the day

--- View/View.cs
+++ View/View.cs
@@ -1,4 +1,6 @@
 using Meetingplanner.Controller;
+using Meetingplanner.Models;
+using System.Linq;
 
 namespace Meetingplanner.View;
 
@@ -14,16 +16,39 @@ public class MeetingView
     public void AddMeeting()
     {
         Console.WriteLine("Enter meeting title:");
-        string Title = Console.ReadLine()?.Trim() ?? "Unknown meeting name";
+        string title = Console.ReadLine()?.Trim() ?? "Unknown meeting name";
 
         Console.WriteLine("Enter start time for meeting  hh-mm");
-        string StartTime = Console.ReadLine()?.Trim() ?? "00.00";
+        string startTime = Console.ReadLine()?.Trim() ?? "00.00";
 
         Console.WriteLine("Enter end of meeting in format HH-hh");
-        string EndTime = Console.ReadLine()?.Trim() ?? "01.00";
+        string endTime = Console.ReadLine()?.Trim() ?? "01.00";
 
-        Console.WriteLine("Who is participating in the meeting?");
-        string Participants = Console.ReadLine()?.Trim() ?? "Unknown";
+        Console.WriteLine("Who is participating in the meeting?(seperate with','");
+        List<string> participants = Console.ReadLine().Split(',').Select(p => p.Trim()).ToList();
+
+        var meeting = new Meeting
+        {
+            Title = title,
+            StartTime = startTime,
+
+        };
+    }
+
+    public void DisplayMeetings()
+    {
+        var meetings = _controller.GetAllMeetings();
+
+        if (meetings.Count == 0)
+        {
+            Console.WriteLine("No meetings scheduled today");
+            return;
+        }
+
+        foreach (var meeting in meetings)
+        {
+            DisplayMeeting(meeting);
+        }
     }
 }
 
